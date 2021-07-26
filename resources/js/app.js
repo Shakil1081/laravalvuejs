2ef6266a9c04249324505c2386924873a7aafa73
shakil1081 <shakil@newgen-bd.com>
2021-07-26 12:52:48 +0600

    routing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-require('./bootstrap');
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import './bootstrap';
+import router from "./routes";
+import Index from "./index";
+
+
+window.Vue = require('vue').default;
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-2', require('./components/Example2.vue').default);
+
+Vue.use(VueRouter);
+const app = new Vue({
+    el: '#app',
+    router,
+    components:{
+        'index': Index
+    }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  //mode: 'jit',
+     // These paths are just examples, customize them to match your project structure
+     purge: [
+      './resources/**/*.blade.php',
+      './resources/**/*.js',
+      './resources/**/*.vue',
+    ],
+     darkMode: false, // or 'media' or 'class'
+     theme: {
+       extend: {},
+     },
+     variants: {
+       extend: {},
+     },
+     plugins: [],
+   }
